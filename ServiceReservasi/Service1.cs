8a31237cf77e8c20d8b0ee7aa8f66c0f2a85fcf3
alfianhidayahh <62759588+alfianhidayahh@users.noreply.github.com>
2020-11-26 00:03:28 +0700

    Add project files.

---
+++ ServiceReservasi/Service1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceReservasi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        string constr = "Data Source=DESKTOP-3M69Q4U; Initial Catalog=WCFReservasi; Persist Security Info=True; User ID=sa; Password=Pbun2018";
+        SqlConnection connection;
+        SqlCommand com;
+
+        public List<DetailLokasi> DetailLokasi()
+        {
+            List<DetailLokasi> lokasiFull = new List<DetailLokasi>();
+            try
+            {
+                string sql = "select ID_lokasi, Nama_lokasi, Deskripsi_full, Kuota from dbo.Lokasi";
+                connection = new SqlConnection(constr);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    DetailLokasi data = new DetailLokasi();
+                    data.IDLokasi = reader.GetString(0);
+                    data.NamaLokasi = reader.GetString(1);
+                    data.DeskripsiFull = reader.GetString(2);
+                    data.Kouta = reader.GetInt32(3);
+                    lokasiFull.Add(data);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return lokasiFull;
+        }
+
+        public string pemesanan(string IDPemesanan, string NamaCustomer, string Notelpon, int JumlahPemesanan, string IDLokasi)
+        {
+            string a = "gagal";
+            try
+            {
+                string sql = "insert into dbo.Pemesanan values('" + IDPemesanan + "', '" + NamaCustomer + "', '" + Notelpon + "', " + JumlahPemesanan + ", '" + IDLokasi + "')";
+                connection = new SqlConnection(constr);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                a = "sukses";
+
+            }
+            catch (Exception ep)
+            {
+                Console.WriteLine(ep);
+            }
+            return a;
+        }
+        //public string GetData(int value)
+        //{
+            //return string.Format("You entered: {0}", value);
+        //}
+
+        //public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //{
+            //if (composite == null)
+            //{
+                //throw new ArgumentNullException("composite");
+            //}
+            //if (composite.BoolValue)
+            //{
+                //composite.StringValue += "Suffix";
+            //}
+            //return composite;
+        //}
+
+        public string editPemesanan(string IDPemesanan, string NamaCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string deletePemesann(string IDPemesanan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CekLokasi> ReviewLokasi()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pemesanan> Pemesanan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
